8a99cae6d516343129bd345bcf93b7e044f19c59
YellaFlame <leonardo.rosenbaum@hotmail.com>
2021-07-29 13:24:00 -0300

    Rotas de entrar e cadastrar criadas

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,10 @@
+
+export class Userlogin {
+    public Id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public token: string
+    public foto: string 
+    public tipo: string
+}
